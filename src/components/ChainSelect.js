40b1fa080530897af8eedea936ab2e983236b295
carlos-bsc <carlos-bsc@protonmail.com>
2021-07-24 18:10:29 +0200

    temporarily remove eth support

--- src/components/ChainSelect.js
+++ src/components/ChainSelect.js
@@ -7,7 +7,8 @@ function ChainSelect(props) {
     return <Select defaultValue="bsc" style={{ width: "100%", height:"100%"}} onChange={onSelect} disabled={disabled}>
         <Option value="bsc">Binance Smart Chain</Option>
         <Option value="poly">Polygon</Option>
-        <Option value="eth">Ethereum</Option>
+        {//<Option value="eth">Ethereum</Option>
+        }
     </Select>
 
 }
