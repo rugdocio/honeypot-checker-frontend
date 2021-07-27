4d54a390eed875d115593069a80aab13b628cadc
jambestwick <jambestwick1988@gmial.com>
2021-07-27 11:07:39 +0800

    add ftm chain on select

--- src/components/ChainSelect.js
+++ src/components/ChainSelect.js
@@ -1,15 +1,15 @@
-import { Select } from 'antd';
+import { Select } from "antd";
 
 const { Option } = Select;
 
 function ChainSelect(props) {
-    const { onSelect, disabled } = props;
-    return <Select defaultValue="bsc" style={{ width: "100%", height:"100%"}} onChange={onSelect} disabled={disabled}>
-        <Option value="bsc">Binance Smart Chain</Option>
-        <Option value="poly">Polygon</Option>
-        {//<Option value="eth">Ethereum</Option>
-        }
-    </Select>
+  const { onSelect, disabled } = props;
+  return <Select defaultValue="bsc" style={{ width: "100%", height: "100%" }} onChange={onSelect} disabled={disabled}>
+    <Option value="bsc">Binance Smart Chain</Option>
+    <Option value="poly">Polygon</Option>
+    <Option value="eth">Ethereum</Option>
+    <Option value="ftm">Fantom </Option>
+  </Select>;
 
 }
 
--- src/config/constants/endpoints.js
+++ src/config/constants/endpoints.js
@@ -4,5 +4,6 @@ export const HONEYPOT_API_ENDPOINT="https://honeypot.api.rugdoc.io/api/honeypotS
 export const EXPLORER_ENDPOINTS = {
     "poly": "https://api.polygonscan.com",
     "bsc": "https://api.bscscan.com",
-    "eth": "https://api.etherscan.com"
+    "eth": "https://api.etherscan.com",
+    "ftm": "https://api.ftmscan.com"
 }
